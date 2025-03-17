790ce6bc82d522c15446b3c2742c279dc9a7d9ba
BenCampbellNelson <campbellnelsonb@gmail.com>
2025-03-17 14:46:43 +1300

    feat(javascript): implemented route protection for admin pages in router configuration

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,27 +1,33 @@
-import { createBrowserRouter, Outlet} from 'react-router-dom';
-import { Layout } from '../Component/Layout/Layout';
-import {routes} from './routeList';
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import { Layout } from "../Component/Layout/Layout";
+import { routes } from "./routeList";
 
 const AppLayout = () => {
-    return (
+  console.log("Rendering AppLayout");
+  return (
     <>
-    <Layout/>
-    <Outlet/>
+      <Layout />
+      <Outlet />
     </>
-    )
+  );
 };
 
 const routerConfig = [
-    {
-        element: <AppLayout/>,
-        // errorElement: "ImplementErrorPage",
-        children: routes.map((route)=> ({
-            path: route.path,
-            element: route.element,
-        }))
-    }
+  {
+    element: <AppLayout />,
+    children: routes.map((route) => {
+      console.log("Adding route:", route.path);
+      return {
+        path: route.path,
+        element: route.element,
+        children: route.children || [], 
+      };
+    }),
+  },
 ];
 
 const router = createBrowserRouter(routerConfig);
+
+console.log("Router configuration initialized:", routerConfig);
 
 export default router;

37854a6187a2517ad68aada401f24ee4d3ae489f
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-12 17:02:17 +1200

    fix: offline status when no data available

--- frontend/src/Component/Tables/TableSubComponents/Admin/AdminTableBody.jsx
+++ frontend/src/Component/Tables/TableSubComponents/Admin/AdminTableBody.jsx
@@ -29,38 +29,56 @@ const AdminTableBody = ({ tableFields, updateTableData }) => {
   return (
     <>
       <tbody className="bg-white divide-y divide-gray-200">
-        {tableFields.map((item) => {
-          const { topText, bottomText } = formatDate(item.lastSeen);
-          return (
-            <tr key={item.id} className="hover:bg-gray-200 odd:bg-gray-50">
-              <td className="pl-1 py-2 text-gray-900">
-                <div className="font-semibold">{item.deviceId}</div>
-                <div className="text-gray-500 italic">{item.dev_eui}</div>
-              </td>
-              <TableItem item={item.room_number} />
-              <TableItem item={item.blockName} />
-              <td className="pl-1 py-2 text-gray-900">
-                <div className="font-semibold">{topText}</div>
-                <div className="text-gray-500 italic">{bottomText}</div>
-              </td>
-              <TableItem
-                item={checkOfflineDate(item.lastSeen) ? 'Offline' : 'Online'}
-                className={`${
-                  checkOfflineDate(item.lastSeen)
-                    ? 'text-red-500'
-                    : 'text-green-500'
-                }`}
-              />
-              <td className="text-right pr-2 py-2 flex justify-end">
-                <UpdateButton
-                  text="Edit"
-                  style="py-2 px-4 text-white bg-blue-500 rounded-lg"
-                  onClick={() => handleEditClick(() => 'edit', item)} // Pass the current item
-                />
-              </td>
-            </tr>
-          );
-        })}
+        {tableFields.length === 0 ? (
+          <tr>
+            <td colSpan="5" className="text-center py-4 text-gray-500">
+              No devices found.
+            </td>
+          </tr>
+        ) : (
+          <>
+            {tableFields.map((item) => {
+              const { topText, bottomText } = formatDate(item.lastSeen);
+              return (
+                <tr key={item.id} className="hover:bg-gray-200 odd:bg-gray-50">
+                  <td className="pl-1 py-2 text-gray-900">
+                    <div className="font-semibold">{item.deviceId}</div>
+                    <div className="text-gray-500 italic">{item.dev_eui}</div>
+                  </td>
+                  <TableItem item={item.room_number} />
+                  <TableItem item={item.blockName} />
+                  <td className="pl-1 py-2 text-gray-900">
+                    <div className="font-semibold">{topText}</div>
+                    <div className="text-gray-500 italic">{bottomText}</div>
+                  </td>
+                  <TableItem
+                    item={
+                      !item.lastSeen || checkOfflineDate(item.lastSeen)
+                        ? 'Offline'
+                        : 'Online'
+                    }
+                    className={
+                      item.lastSeen
+                        ? `${
+                            checkOfflineDate(item.lastSeen)
+                              ? 'text-red-500'
+                              : 'text-green-500'
+                          }`
+                        : 'text-red-500'
+                    }
+                  />
+                  <td className="text-right pr-2 py-2 flex justify-end">
+                    <UpdateButton
+                      text="Edit"
+                      style="py-2 px-4 text-white bg-blue-500 rounded-lg"
+                      onClick={() => handleEditClick(() => 'edit', item)} // Pass the current item
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </>
+        )}
       </tbody>
       {modal &&
         selectedItem && ( // Ensure selectedItem is defined

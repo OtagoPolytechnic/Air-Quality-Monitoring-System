d1086ebd12efe8edca0f60222468483566b88327
Bernie644 <bcosgrove217@gmail.com>
2025-05-15 11:38:59 +1200

    added practice selenium test

---
+++ frontend/Air-Quality-Monitoring-System-Tests/Air-Quality-Monitoring-System-Tests/UnitTest2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+
+namespace Air_Quality_Monitoring_System_Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (IWebDriver wdriver = new ChromeDriver())
+            {
+                wdriver.Navigate().GoToUrl("https://co2-app.op-bit.nz/");
+                wdriver.Manage().Window.Maximize();
+
+                wdriver.FindElement(By.ClassName("text-center")).Click();
+                Assert.AreEqual(wdriver.Url, "https://co2-app.op-bit.nz/D-Block");
+
+            }
+        }
+    }
+}
